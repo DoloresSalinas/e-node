5281aa617354e1228f9b470f31c8f7ee73d1f0c4
Dolores <maria2doloresjimenez@gmail.com>
2025-10-07 22:15:16 -0600

    primer commit

---
+++ test/testindex.js
@@ -0,0 +1,10 @@
+import crypto from "crypto";
+
+function hash(mensaje){
+    const hash = crypto.createHash('sha256');
+    hash.update(mensaje);
+    return hash.digest('hex');
+    
+}
+
+module.exports = hash;
